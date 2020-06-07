8052876e304417a98f4b0b0b2ff8c716f5ad4fdd
zhangguangxu <zhangguangxu@joycastle.mobi>
2020-06-07 16:33:16 +0800

    insertion sort

---
+++ internal/insertion_sort/sort.go
@@ -0,0 +1,21 @@
+package insertion_sort
+
+// in-place
+// stable
+// time: 最好O(n)，最坏O(n^2)，平均O(n^2)
+// space: O(1)
+func Sort(a []int) {
+    l := len(a)
+    if l < 2 {
+        return
+    }
+    for i := 1; i < l; i++ {
+        x := a[i]
+        j := i-1
+        for j >= 0 && a[j] > x {
+            a[j+1] = a[j]
+            j--
+        }
+        a[j+1] = x
+    }
+}
