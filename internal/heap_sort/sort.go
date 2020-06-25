93b405dd418f3ead9776f8e82d6e10638aeca2a1
zhangguangxu <zhangguangxu@joycastle.mobi>
2020-06-25 21:22:53 +0800

    heap sort

---
+++ internal/heap_sort/sort.go
@@ -0,0 +1,34 @@
+package heap_sort
+
+// in-place
+// unstable
+// time: 最好O(nlogn)，最坏O(nlogn)，平均O(nlogn)
+// space: O(1)
+func Sort(a []int) {
+	if len(a) < 2 {
+		return
+	}
+	for i := len(a)/2; i >= 0; i-- {
+		heapAdjust(a, i, len(a)-1)
+	}
+	for i := len(a)-1; i > 0; i-- {
+		a[0], a[i] = a[i], a[0]
+		heapAdjust(a, 0, i-1)
+	}
+}
+
+func heapAdjust(a []int, start, end int) {
+	temp := a[start]
+	top := start
+	for i := 2*top+1; i <= end; i = 2*top+1 {
+		if i < end && a[i] < a[i+1] {
+			i++
+		}
+		if temp > a[i] {
+			break
+		}
+		a[top] = a[i]
+		top = i
+	}
+	a[top] = temp
+}
---
+++ internal/heap_sort/sort_test.go
@@ -0,0 +1,54 @@
+package heap_sort
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestSort(t *testing.T) {
+	{
+		var a []int
+		Sort(a)
+		t.Log(a)
+	}
+	{
+		a := make([]int, 0)
+		Sort(a)
+		t.Log(a)
+	}
+	{
+		a := []int{1}
+		Sort(a)
+		t.Log(a)
+	}
+	{
+		a := []int{1, 2, 3}
+		Sort(a)
+		t.Log(a)
+	}
+	{
+		a := []int{9, 8, 7, 6, 5, 4, 3, 2}
+		Sort(a)
+		t.Log(a)
+	}
+	{
+		a := []int{90, 8, 70, 25, 16, 52, 34, 41, 31, 62, 19, 88, 1}
+		Sort(a)
+		t.Log(a)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	{
+		const L = 50
+		a := make([]int, 0, L)
+		for i := 0; i < L; i++ {
+			r := rand.Intn(99999)
+			a = append(a, r)
+		}
+		t.Logf("before, %v", a)
+		Sort(a)
+		t.Logf("after, %v", a)
+	}
+}
